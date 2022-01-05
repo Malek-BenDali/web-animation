e6392a095ddb18a02c2032ac42d6522bca639f60
Malek Ben Dali <bendali.malekk@gmail.com>
2022-01-05 22:23:35 +0100

    added macroanimation section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Cookie from './projects/cookies/Cookie'
 import TextAnimation from './projects/textAnimation/TextAnimation'
+import MacroAnimation from './projects/macroAnimation/MacroAnimation'
 
 function App() {
 	// return <Cookie />
-	return <TextAnimation />
+	// return <TextAnimation />
+	return <MacroAnimation />
 }
 
 export default App
