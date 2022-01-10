bc28598872899a07c7287e7d796874d6e958bf5d
Malek Ben Dali <bendali.malekk@gmail.com>
2022-01-10 03:53:07 +0100

    added new sections

--- src/projects/ultimate/Ultimate.jsx
+++ src/projects/ultimate/Ultimate.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ultimate.css'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
-import { introAnimation, smokeVideo } from './images'
+import {
+	introAnimation,
+	smokeVideo,
+	largePhone,
+	smallPhone,
+	blueIphoneBack,
+	silverIphoneBack,
+	goldIphoneBack,
+	graphiteIphoneBack,
+	swatchBlue,
+	swatchSilver,
+	swatchGold,
+	swatchGraphite,
+	blueCloseup,
+	silverCloseup,
+	goldCloseup,
+	graphiteCloseup,
+} from './images'
 
 function Ultimate() {
 	gsap.registerPlugin(ScrollTrigger)
+	const [swatch, setSwatch] = useState('blue')
 	return (
 		<>
 			<nav className="nav-ultimate">
@@ -48,6 +66,69 @@ function Ultimate() {
 						<br /> <br />
 						<span className="highlight">Let's pro</span>
 					</p>
+				</div>
+			</section>
+			<section className="third-page">
+				<div className="product-text-container">
+					<div className="product-text-left">
+						<p className="phone-title">iPhone 13 Pro Max</p>
+						<p className="phone-size pro-size">6.7"</p>
+					</div>
+					<div className="product-text-right">
+						<div className="phone-title">iPhone 13 Max</div>
+						<div className="phone-size max-size">6.1"</div>
+					</div>
+				</div>
+				<div className="product-images">
+					<img src={largePhone} className="large-phone" alt="a" />
+					<img src={smallPhone} className="small-phone" alt="a" />
+				</div>
+				<p className="retina-line">SuperRetina XDR display with ProMotion</p>
+			</section>
+			<section className="forth-page">
+				<div className="purchase-left">
+					<div className="phone-gallery">
+						<div className="phone-gallery-container blue-cont ">
+							<img src={blueIphoneBack} className="blue-back" alt="" />
+						</div>
+						<div className="phone-gallery-container blue-cont ">
+							<img src={silverIphoneBack} className="silver-back" alt="" />
+						</div>
+						<div className="phone-gallery-container blue-cont ">
+							<img src={goldIphoneBack} className="gold-back" alt="" />
+						</div>
+						<div className="phone-gallery-container blue-cont ">
+							<img src={graphiteIphoneBack} className="graphite-back" alt="" />
+						</div>
+					</div>
+					<div className="swatch-container">
+						<div className="swatches">
+							<img src={swatchBlue} alt="s" onClick={() => setSwatch('blue')} />
+							<img
+								src={swatchSilver}
+								alt="s"
+								onClick={() => setSwatch('silver')}
+							/>
+							<img src={swatchGold} alt="s" onClick={() => setSwatch('gold')} />
+							<img
+								src={swatchGraphite}
+								alt="s"
+								onClick={() => setSwatch('graphite')}
+							/>
+						</div>
+						<p>
+							<span>Sierra Blue </span>
+							<span>Silver </span>
+							<span>Gold </span>
+							<span>Graphite </span>
+						</p>
+					</div>
+				</div>
+				<div className="purchase-right">
+					<img src={blueCloseup} className="blue phone" alt="a" />
+					<img src={silverCloseup} className="silver phone" alt="a" />
+					<img src={goldCloseup} className="gold phone" alt="a" />
+					<img src={graphiteCloseup} className="graphite phone" alt="a" />
 				</div>
 			</section>
 		</>
--- src/projects/ultimate/images/index.js
+++ src/projects/ultimate/images/index.js
@@ -1,3 +1,35 @@
 import introAnimation from './intro-animation.mov'
 import smokeVideo from './smoke-video.mp4'
-export { introAnimation, smokeVideo }
+import largePhone from './large-phone.png'
+import smallPhone from './small-phone.png'
+import blueIphoneBack from './blue-iphone-back.png'
+import silverIphoneBack from './silver-iphone-back.png'
+import graphiteIphoneBack from './graphite-iphone-back.png'
+import goldIphoneBack from './gold-iphone-back.png'
+import swatchBlue from './swatch-blue.svg'
+import swatchSilver from './swatch-silver.svg'
+import swatchGold from './swatch-gold.svg'
+import swatchGraphite from './swatch-graphite.svg'
+import blueCloseup from './blue-closeup.png'
+import silverCloseup from './silver-closeup.png'
+import goldCloseup from './gold-closeup.png'
+import graphiteCloseup from './graphite-closeup.png'
+
+export {
+	introAnimation,
+	smokeVideo,
+	largePhone,
+	smallPhone,
+	blueIphoneBack,
+	goldIphoneBack,
+	graphiteIphoneBack,
+	silverIphoneBack,
+	swatchBlue,
+	swatchGold,
+	swatchGraphite,
+	swatchSilver,
+	blueCloseup,
+	goldCloseup,
+	graphiteCloseup,
+	silverCloseup,
+}
